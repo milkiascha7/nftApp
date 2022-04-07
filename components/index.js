75a30a9eb412481d8a335157d1eda6ddb1967ce3
Milkias Bekele <mikehailu504@gmail.com>
2022-04-07 10:57:38 +0300

    first commit

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import HomeHeader from "./HomeHeader";
+import FocusedState from "./FocusedState";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
+
+export { HomeHeader, NFTCard, FocusedState, CircleButton, RectButton, DetailsDesc, DetailsBid, SubInfo }
